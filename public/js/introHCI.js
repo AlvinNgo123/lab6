3b0d099bdec6bc568c43a565b15e0f13e3af6b2c
Alvin Ngo <aln011@ucsd.edu>
2018-02-15 11:25:52 -0800

    changes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,18 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get('/project/' + idNumber, addProject)
 
 	console.log("User clicked on project " + idNumber);
 }
+
+function addProject(result) {
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+
+  $("#project" + result.id + " .details").html(projectHTML);
+}
+
